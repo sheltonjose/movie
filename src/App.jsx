f4c24a4572a97ecd44f8a5242d9fdc7a5ed9c31f
Shelton <198606617+sheltonjose@users.noreply.github.com>
2026-09-04 16:23:02 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
 function App() {
 
 
   return (
-      <><h1>Hello world</h1>
-      
+      <>
+      <Header/>
+      <h1>Hello world</h1>
+      <p>Hello World, i'm learning React.JS</p>
+      <Footer/>
       </>
     
   )
