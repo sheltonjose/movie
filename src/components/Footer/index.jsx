c8d98e17719c93a5a5334136544f24a63c464c7b
Shelton <198606617+sheltonjose@users.noreply.github.com>
2026-09-04 16:23:09 +0200

    Create index.jsx

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer(){
+    return(
+        <footer className={styles.footer}>
+            <h2>MaiaFlix &copy; Developed by Shelton José 2026</h2>
+        </footer>
+    );
+}
+
+export default Footer;
