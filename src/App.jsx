fbdef47a9bfc3fa84db371304b3b410668daf1a0
Shelton <198606617+sheltonjose@users.noreply.github.com>
2026-09-07 16:42:14 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Banner from "./components/Banner"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 
@@ -7,8 +8,19 @@ function App() {
   return (
       <>
       <Header/>
+      <Banner/>
       <h1>Hello world</h1>
       <p>Hello World, i'm learning React.JS</p>
+      <p>lo</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum vitae quam corporis cum ipsam architecto amet, commodi eligendi suscipit aspernatur ullam doloribus reprehenderit natus esse beatae accusantium alias vel.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aperiam obcaecati, inventore ab optio ut. Omnis dolor eaque cum molestiae, non, laudantium quas suscipit obcaecati illo eius beatae quo nam.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis explicabo nulla, facilis, esse tempore animi enim voluptatum earum ex quidem rerum, quod quae eum nisi. Dolorum adipisci repellat eius fugit.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta esse porro dignissimos id voluptas inventore, quasi minima maiores error nisi commodi necessitatibus omnis temporibus consequuntur magnam, officia, tenetur laudantium accusantium.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas aperiam cupiditate id cumque ex labore quis nobis quasi sunt deserunt vitae pariatur nulla laboriosam, rem dignissimos. Vitae nisi magni quisquam!</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo necessitatibus tempora dolore fugit harum ipsum obcaecati soluta dignissimos! Aliquam, ad explicabo? Excepturi suscipit harum saepe cupiditate molestias dolorem vitae quam!</p>sadasdsad
+      <p>asdsad</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt tenetur hic consequuntur impedit? Recusandae facilis animi, soluta saepe asperiores exercitationem magnam, qui quae debitis error molestias amet distinctio quaerat ipsa.</p>
+      <p>lor</p>
       <Footer/>
       </>
     
