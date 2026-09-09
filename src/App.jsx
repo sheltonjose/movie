fa5967e1ce7226365a5117f29af46983a15208ca
Shelton <198606617+sheltonjose@users.noreply.github.com>
2026-09-09 23:18:01 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,67 +11,9 @@ function App() {
       <Header/>
       <Banner/>
       <Container>
-          <h1>Hello world</h1>
-          <p>Hello World, i'm learning React.JS</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum vitae quam corporis cum ipsam architecto amet, commodi eligendi suscipit aspernatur ullam doloribus reprehenderit natus esse beatae accusantium alias vel.</p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus error omnis quos unde dolores similique at voluptatum a inventore explicabo repellendus velit architecto, impedit itaque est quo. Eos, quasi.</p>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae dolore quaerat, autem saepe commodi totam repellat quas, corporis perferendis aspernatur aperiam nihil voluptatem excepturi amet? Nisi quo temporibus culpa ex?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores expedita esse tempora consectetur! Suscipit sit vel temporibus repellat in corporis asperiores nobis atque, eius rem, dolorem recusandae, quo quisquam non.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque doloremque eligendi dicta obcaecati dolores et soluta, consequatur laboriosam molestiae quidem eius non itaque quod facilis! Illo non ab possimus harum.</p>
-            <p>Hello World, i'm learning React.JS</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum vitae quam corporis cum ipsam architecto amet, commodi eligendi suscipit aspernatur ullam doloribus reprehenderit natus esse beatae accusantium alias vel.</p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus error omnis quos unde dolores similique at voluptatum a inventore explicabo repellendus velit architecto, impedit itaque est quo. Eos, quasi.</p>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae dolore quaerat, autem saepe commodi totam repellat quas, corporis perferendis aspernatur aperiam nihil voluptatem excepturi amet? Nisi quo temporibus culpa ex?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores expedita esse tempora consectetur! Suscipit sit vel temporibus repellat in corporis asperiores nobis atque, eius rem, dolorem recusandae, quo quisquam non.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque doloremque eligendi dicta obcaecati dolores et soluta, consequatur laboriosam molestiae quidem eius non itaque quod facilis! Illo non ab possimus harum.</p>
-            <p>Hello World, i'm learning React.JS</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum vitae quam corporis cum ipsam architecto amet, commodi eligendi suscipit aspernatur ullam doloribus reprehenderit natus esse beatae accusantium alias vel.</p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus error omnis quos unde dolores similique at voluptatum a inventore explicabo repellendus velit architecto, impedit itaque est quo. Eos, quasi.</p>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae dolore quaerat, autem saepe commodi totam repellat quas, corporis perferendis aspernatur aperiam nihil voluptatem excepturi amet? Nisi quo temporibus culpa ex?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores expedita esse tempora consectetur! Suscipit sit vel temporibus repellat in corporis asperiores nobis atque, eius rem, dolorem recusandae, quo quisquam non.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque doloremque eligendi dicta obcaecati dolores et soluta, consequatur laboriosam molestiae quidem eius non itaque quod facilis! Illo non ab possimus harum.</p>
-            <p>Hello World, i'm learning React.JS</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum vitae quam corporis cum ipsam architecto amet, commodi eligendi suscipit aspernatur ullam doloribus reprehenderit natus esse beatae accusantium alias vel.</p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus error omnis quos unde dolores similique at voluptatum a inventore explicabo repellendus velit architecto, impedit itaque est quo. Eos, quasi.</p>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae dolore quaerat, autem saepe commodi totam repellat quas, corporis perferendis aspernatur aperiam nihil voluptatem excepturi amet? Nisi quo temporibus culpa ex?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores expedita esse tempora consectetur! Suscipit sit vel temporibus repellat in corporis asperiores nobis atque, eius rem, dolorem recusandae, quo quisquam non.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque doloremque eligendi dicta obcaecati dolores et soluta, consequatur laboriosam molestiae quidem eius non itaque quod facilis! Illo non ab possimus harum.</p>
-            <p>Hello World, i'm learning React.JS</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum vitae quam corporis cum ipsam architecto amet, commodi eligendi suscipit aspernatur ullam doloribus reprehenderit natus esse beatae accusantium alias vel.</p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus error omnis quos unde dolores similique at voluptatum a inventore explicabo repellendus velit architecto, impedit itaque est quo. Eos, quasi.</p>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae dolore quaerat, autem saepe commodi totam repellat quas, corporis perferendis aspernatur aperiam nihil voluptatem excepturi amet? Nisi quo temporibus culpa ex?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores expedita esse tempora consectetur! Suscipit sit vel temporibus repellat in corporis asperiores nobis atque, eius rem, dolorem recusandae, quo quisquam non.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque doloremque eligendi dicta obcaecati dolores et soluta, consequatur laboriosam molestiae quidem eius non itaque quod facilis! Illo non ab possimus harum.</p>
-            <p>Hello World, i'm learning React.JS</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum vitae quam corporis cum ipsam architecto amet, commodi eligendi suscipit aspernatur ullam doloribus reprehenderit natus esse beatae accusantium alias vel.</p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus error omnis quos unde dolores similique at voluptatum a inventore explicabo repellendus velit architecto, impedit itaque est quo. Eos, quasi.</p>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae dolore quaerat, autem saepe commodi totam repellat quas, corporis perferendis aspernatur aperiam nihil voluptatem excepturi amet? Nisi quo temporibus culpa ex?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores expedita esse tempora consectetur! Suscipit sit vel temporibus repellat in corporis asperiores nobis atque, eius rem, dolorem recusandae, quo quisquam non.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque doloremque eligendi dicta obcaecati dolores et soluta, consequatur laboriosam molestiae quidem eius non itaque quod facilis! Illo non ab possimus harum.</p>
-            <p>Hello World, i'm learning React.JS</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum vitae quam corporis cum ipsam architecto amet, commodi eligendi suscipit aspernatur ullam doloribus reprehenderit natus esse beatae accusantium alias vel.</p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus error omnis quos unde dolores similique at voluptatum a inventore explicabo repellendus velit architecto, impedit itaque est quo. Eos, quasi.</p>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae dolore quaerat, autem saepe commodi totam repellat quas, corporis perferendis aspernatur aperiam nihil voluptatem excepturi amet? Nisi quo temporibus culpa ex?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores expedita esse tempora consectetur! Suscipit sit vel temporibus repellat in corporis asperiores nobis atque, eius rem, dolorem recusandae, quo quisquam non.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque doloremque eligendi dicta obcaecati dolores et soluta, consequatur laboriosam molestiae quidem eius non itaque quod facilis! Illo non ab possimus harum.</p>
-            <p>Hello World, i'm learning React.JS</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum vitae quam corporis cum ipsam architecto amet, commodi eligendi suscipit aspernatur ullam doloribus reprehenderit natus esse beatae accusantium alias vel.</p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus error omnis quos unde dolores similique at voluptatum a inventore explicabo repellendus velit architecto, impedit itaque est quo. Eos, quasi.</p>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae dolore quaerat, autem saepe commodi totam repellat quas, corporis perferendis aspernatur aperiam nihil voluptatem excepturi amet? Nisi quo temporibus culpa ex?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores expedita esse tempora consectetur! Suscipit sit vel temporibus repellat in corporis asperiores nobis atque, eius rem, dolorem recusandae, quo quisquam non.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque doloremque eligendi dicta obcaecati dolores et soluta, consequatur laboriosam molestiae quidem eius non itaque quod facilis! Illo non ab possimus harum.</p>
-            <p>Hello World, i'm learning React.JS</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum vitae quam corporis cum ipsam architecto amet, commodi eligendi suscipit aspernatur ullam doloribus reprehenderit natus esse beatae accusantium alias vel.</p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus error omnis quos unde dolores similique at voluptatum a inventore explicabo repellendus velit architecto, impedit itaque est quo. Eos, quasi.</p>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae dolore quaerat, autem saepe commodi totam repellat quas, corporis perferendis aspernatur aperiam nihil voluptatem excepturi amet? Nisi quo temporibus culpa ex?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores expedita esse tempora consectetur! Suscipit sit vel temporibus repellat in corporis asperiores nobis atque, eius rem, dolorem recusandae, quo quisquam non.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque doloremque eligendi dicta obcaecati dolores et soluta, consequatur laboriosam molestiae quidem eius non itaque quod facilis! Illo non ab possimus harum.</p>
-            <p>Hello World, i'm learning React.JS</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum vitae quam corporis cum ipsam architecto amet, commodi eligendi suscipit aspernatur ullam doloribus reprehenderit natus esse beatae accusantium alias vel.</p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga accusamus error omnis quos unde dolores similique at voluptatum a inventore explicabo repellendus velit architecto, impedit itaque est quo. Eos, quasi.</p>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae dolore quaerat, autem saepe commodi totam repellat quas, corporis perferendis aspernatur aperiam nihil voluptatem excepturi amet? Nisi quo temporibus culpa ex?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores expedita esse tempora consectetur! Suscipit sit vel temporibus repellat in corporis asperiores nobis atque, eius rem, dolorem recusandae, quo quisquam non.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque doloremque eligendi dicta obcaecati dolores et soluta, consequatur laboriosam molestiae quidem eius non itaque quod facilis! Illo non ab possimus harum.</p>
+        <h1>hello world</h1>
+        <p>Olá Mundo, estou aprendendo React JS!</p>
+          
      </Container>
       <Footer/>
       </>
