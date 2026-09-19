d83841126ad39798754fe3fa262f8a6ec22aae86
Shelton <198606617+sheltonjose@users.noreply.github.com>
2026-09-19 14:17:38 +0200

    Update index.jsx

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,13 +1,13 @@
 import styles from './Card.module.css';
 
-function Card(){
+function Card({id}){
     return (
         <section className={styles.card}>
             <a
-                 href="https://WWW.youtube.com/watch?v=B_nq7VTJZds"
+                 href={`https://WWW.youtube.com/watch?v=${id}`}
                  rel="noreferrer noopener"  
                  target='_blank'>
-                <img src="https://img.youtube.com/vi/B_nq7VTJZds/mqdefault.jpg" alt="Capa do vídeo" />
+                <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`} alt="Capa do vídeo" />
             </a>
         </section>
     );
