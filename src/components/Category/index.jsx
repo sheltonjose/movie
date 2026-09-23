5ff7e4db779e6541969632aa690405d4af9f5cc8
Shelton <198606617+sheltonjose@users.noreply.github.com>
2026-09-24 00:32:29 +0200

    Create index.jsx

---
+++ src/components/Category/index.jsx
@@ -0,0 +1,16 @@
+import styles from "./Category.module.css"
+
+function Category({category, children}){
+    return(
+    
+     <section className={styles.category}>
+            <h2>{category}</h2>
+            <div>  
+                {children}
+            </div>
+     </section>
+
+    );
+}
+
+export default Category;
