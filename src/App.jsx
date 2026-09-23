64bb54dced7979836ca526d939cc0dadc880a117
Shelton <198606617+sheltonjose@users.noreply.github.com>
2026-09-23 15:12:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Card from "./components/Card"
 import Container from "./components/Container"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
+import Videos  from "./JSON/db.json"
 
 function App() {
 
@@ -12,14 +13,17 @@ function App() {
       <Header/>
       <Banner image="Home" />
       <Container>
+
         <h2>Georgrafia</h2>
-       <section className="cards">
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+
+       <section className="cards">  
+        {
+          Videos.map(video =>{
+            return <Card id={video.id} key={video.id}/>
+          })
+        }
        </section>
+
      </Container>
       <Footer/>
       </>
