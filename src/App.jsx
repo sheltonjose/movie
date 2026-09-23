97b435bd0f95a9299a2b068e215269ccc1b9bf29
Shelton <198606617+sheltonjose@users.noreply.github.com>
2026-09-23 23:02:35 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Card from "./components/Card"
 import Container from "./components/Container"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
-import Videos  from "./JSON/db.json"
+import Videos  from "./JSON/videos.json"
 
 function App() {
 
